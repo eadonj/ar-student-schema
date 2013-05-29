de0c695467ca37888673a1246c301c76d5d2daee
Apprentice <apprentice@dbc04.local>
2013-05-29 16:07:06 -0700

    rocked it

--- Rakefile
+++ Rakefile
@@ -23,6 +23,7 @@ task "db:migrate" do
   end
 end
 
+
 desc "populate the test database with sample data"
 task "db:populate" do
   StudentsImporter.import
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,18 @@
 require_relative '../../db/config'
 
-# implement your Student model here
+class Student < ActiveRecord::Base
+  
+  validates_format_of :email, :with => /.+@+.+[\.]+..+/
+  validates_uniqueness_of :email
+  validates_numericality_of :age, :greater_than_or_equal_to => 5
+  validates_format_of :phone, :with => /\d+/
+  validates_length_of :phone, :minimum => 10
+                  
+  def name
+    self.first_name + " " + self.last_name
+  end
+
+  def age
+    @age = Date.today.year - self.birthday.year - ((Date.today.month > self.birthday.month || (Date.today.month == self.birthday.month && Date.today.day >= self.birthday.day)) ? 0 : 1 )
+  end
+end
